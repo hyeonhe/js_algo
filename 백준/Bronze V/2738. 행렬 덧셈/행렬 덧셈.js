e5829d0c9c1e1798c618204e0d806918232ac7d2
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-02-14 17:34:11 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 136 ms, Memory: 10236 KB -BaekjoonHub

--- 백준/Bronze V/2738. 행렬 덧셈/행렬 덧셈.js
+++ 백준/Bronze V/2738. 행렬 덧셈/행렬 덧셈.js
@@ -1,19 +1,10 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
-const [n, m] = input[0].split(" ").map((i) => Number(i));
-input.shift();
+const [n, m] = input.shift().split(" ").map((i) => Number(i));
 
-const A = input
-  .splice(0, n)
-  .map((item) => item.split(" ").map((i) => Number(i)));
+const A = input.splice(0, n).map((item) => item.split(" ").map((i) => Number(i)));
 const B = input.map((item) => item.split(" ").map((i) => Number(i)));
 
 for (let i = 0; i < n; i++) {
-  let ans = "";
-  for (let j = 0; j < m; j++) {
-    ans += String(A[i][j] + B[i][j]) + " ";
-  }
-
-  console.log(ans);
-  ans = "";
+  console.log(A[i].map((a, j) => a + B[i][j]).join(" "));
 }
