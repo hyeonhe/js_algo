0b8cd0a45bc85230ea0a9d4c480f121a1f42d056
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-02-14 15:47:32 +0900

    [Bronze V] Title: X보다 작은 수, Time: 132 ms, Memory: 11048 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -3,12 +3,5 @@ const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 const [n, x] = input[0].split(" ").map((i) => Number(i));
 const arr = input[1].split(" ").map((i) => Number(i));
-const ans = [];
 
-arr.forEach((i) => {
-  if (x > i) {
-    ans.push(i);
-  }
-});
-
-console.log(ans.join(" "));
+console.log(arr.filter((i) => x > i).join(" "));
