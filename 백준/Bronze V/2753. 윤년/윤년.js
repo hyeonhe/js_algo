bab08fef975922de9853ce934972498e530545f7
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-02-15 10:51:51 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze V/2753. 윤년/윤년.js
+++ 백준/Bronze V/2753. 윤년/윤년.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync("/dev/stdin").toString();
 
 const year = Number(input);
 if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-  console.log(1);
+  console.log('1');
 } else {
-  console.log(0);
+  console.log('0');
 }
