0291277c82ebd3bc277e7ddf1da8cbe547910127
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-02-18 13:35:39 +0900

    [Bronze I] Title: 팰린드롬수, Time: 120 ms, Memory: 9428 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
@@ -3,13 +3,9 @@ const input = fs.readFileSync("/dev/stdin", "utf8").toString().trim().split("\n"
 
 input.forEach((num) => {
   if (+num !== 0) {
-    let ans = "yes";
-    for (let i = 0; i < parseInt(num.length / 2); i++) {
-      if (+num[i] !== +num[num.length - 1 - i]) {
-        ans = "no";
-        break;
-      }
-    }
-    console.log(ans);
+    const isPalindrome =
+      num === num.split("").reverse().join("") ? "yes" : "no";
+
+    console.log(isPalindrome);
   }
 });
