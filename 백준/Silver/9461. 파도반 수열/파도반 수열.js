7a52c47eee60c2b287bbb8a7f3f5acf25d74a4fe
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-02-25 10:27:38 +0900

    [Silver III] Title: 파도반 수열, Time: 132 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.js
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -4,15 +4,12 @@ const input = require("fs")
   .trim()
   .split("\n");
 
+input.shift();
 const dp = Array(101).fill(1);
-const answer = [];
 for (let i = 4; i <= 100; i++) {
   dp[i] = dp[i - 2] + dp[i - 3];
 }
 
-input.shift();
 for (let i in input) {
-  answer.push(dp[+input[i]]);
+  console.log(dp[+input[i]]);
 }
-
-console.log(answer.join("\n"));
