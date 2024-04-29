e893331719db708ad9b430584cb464321511e48e
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-04-29 11:59:37 +0900

    [level 1] Title: 과일 장수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,9 @@
+function solution(k, m, score) {
+    let answer = 0;
+    score.sort((a,b) => b - a)
+
+    for (let i = 0; i < score.length; i += m) {
+      if (i + m <= score.length)  answer += (score[i + m - 1]) * m
+    }
+    return answer;
+}
