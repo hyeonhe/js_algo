0396ca3d3e976f1b7b793cba2604e1d5c2f32240
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-05-19 11:10:11 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.10 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,21 +1,15 @@
 function solution(number, limit, power) {
-    var answer = 0;
+    let answer = 0;
     const arr = Array.from(new Array(number), (x, i) => i + 1);
     
-    const weapon = []
-    let count = 0
     arr.forEach((n) => {
+        let count = 0
         for (let i = 1; i * i <= n; i++){
             if (i * i === n) count++;
             else if (n % i === 0) count += 2;
         }
-        weapon.push(count)
-        count = 0
+        answer += count <= limit ? count : power
     })
-    
-    weapon.forEach((n) => {
-        answer += n <= limit ? n : power
-    })
-    
+
     return answer;
 }
