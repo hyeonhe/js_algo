a80c7a314cf444e95d17eedc083827c98db020ef
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-05-30 19:28:56 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,24 @@
+function solution(n, words) {
+    var answer = [0, 0];
+    const set = new Set()
+    let lastWord = ''
+    
+    for (let i=0; i < words.length; i++) {
+        let [a, b] = [0, 0]
+        let quotient = parseInt(i / n)
+        if (set.has(words[i])) {
+            a = i - quotient * n + 1
+            b = quotient + 1
+            return [a, b]
+        } else set.add(words[i])
+        
+        if (i > 0 && (lastWord !== words[i].slice(0, 1))){
+            a = i - (quotient * n) + 1
+            b = quotient + 1
+            return [a, b]
+        }
+        lastWord = words[i].slice(-1)
+    }
+
+    return answer;
+}
