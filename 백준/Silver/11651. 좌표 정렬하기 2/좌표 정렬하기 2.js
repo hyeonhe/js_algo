fa71918310fedf57e63d7de8bcbcd899b5deaee6
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-05-31 19:48:23 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 460 ms, Memory: 54072 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,21 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+input.shift()
+
+const arr = []
+input.forEach(item => {
+  const [a, b] = item.split(' ')
+  arr.push([+a, +b])
+})
+
+arr.sort((a, b) => {
+  if(a[1] === b[1]) {
+    return a[0] - b[0]
+  }
+  else return a[1] - b[1]
+})
+
+console.log(arr.map(i => i.join(' ')).join('\n'))
