ade1e311d107dcc79c38f8f001d0cf897861a712
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-06-25 20:24:28 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 200 ms, Memory: 9656 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -1,12 +1,12 @@
-const n = require("fs").readFileSync("/dev/stdin").toString().trim();
-
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+const n = +input
 const dp = Array.from({ length: 1001 }).fill(0);
 
+dp[0] = 1;
 dp[1] = 1;
-dp[2] = 3;
 
-for (let i = 3; i < dp.length; i++) {
+for (let i = 2; i <= n; i++) {
   dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
 }
 
-console.log(dp[+n])
+console.log(dp[n])
