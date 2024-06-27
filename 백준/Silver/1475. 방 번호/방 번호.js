7624b4165672e1b6827471f11f5dbd48cfe7ec64
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-06-27 14:42:04 +0900

    [Silver V] Title: 방 번호, Time: 112 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.js
+++ 백준/Silver/1475. 방 번호/방 번호.js
@@ -1,11 +1,12 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim();
 
-const numbers = input.replaceAll("9", "6").split("");
-const arr = Array(9).fill(0);
+const numbers = input.split("").map(Number);
+const arr = Array(10).fill(0);
 
 numbers.forEach((num) => {
-  arr[+num]++;
+  arr[num]++;
 });
 
-arr[6] = Math.ceil(arr[6] / 2);
+arr[6] = Math.ceil((arr[6] + arr[9]) / 2);
+arr[9] = 0;
 console.log(Math.max(...arr));
