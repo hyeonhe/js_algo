aa3c06e8d5fd7a561bbb8b98374776eefc898b34
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2024-08-11 16:45:25 +0900

    [level 1] Title: 실패율, Time: 7.81 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,16 +1,16 @@
 function solution(N, stages) {
     const challenger = new Array(N + 2).fill(0)
-    let people = stages.length;
-    const failPercent = []
     for (const stage of stages) {
         challenger[stage]++
     }
 
+    const failPercent = {};
+    let people = stages.length;
     for (let i = 1; i <= N; i++) {
-        failPercent.push([i, challenger[i] / people])
+        failPercent[i] = challenger[i] / people
         people -= challenger[i]
     }
     
-    const answer = failPercent.sort((a, b) => b[1] - a[1])
-    return answer.map(v => v[0]);
+    const answer = Object.entries(failPercent).sort((a, b) => b[1] - a[1])
+    return answer.map(v => Number(v[0]));
 }
