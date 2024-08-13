875668ba11db7e7a9dfd21519ac64b0b668fecf5
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2024-08-13 14:02:02 +0900

    [level 2] Title: 짝지어 제거하기, Time: 73.34 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -2,11 +2,9 @@ function solution(s)
 {
     const stack = []
     for (const c of s) {
-        if (stack.length === 0) stack.push(c)
-        else {
-            if (stack[stack.length - 1] !== c) stack.push(c)
-            else stack.pop()
-        }
+        if (stack.length > 0 && stack[stack.length - 1] === c) {
+            stack.pop()
+        } else stack.push(c)
     }
 
     return stack.length === 0 ? 1 : 0;
