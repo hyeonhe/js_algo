199887911bc0e12acdbc7e14570026d4c2684de7
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2024-08-15 17:27:24 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,19 +1,22 @@
 function solution(board, moves) {
     const stack = []
-    let count = 0
+    const lanes = [...Array(board[0].length)].map(() => [])
     
+    for (let i = board.length - 1; i >= 0; i--) {
+        for (let j = 0; j < board[0].length; j++) {
+            if(board[i][j]) lanes[j].push(board[i][j])
+        }
+    }
+    
+    let count = 0
     for (const move of moves) {
-        for (let i = 0; i < board.length; i++) {
-            const doll = board[i][move-1]
-            if (doll) {
-                if (stack.length === 0 || stack[stack.length - 1] !== doll) stack.push(doll)
-                else {
-                    stack.pop()
-                    count += 2
-                }
-                board[i][move-1] = 0
-                break
-            }
+        if (lanes[move-1].length > 0) {
+            const doll = lanes[move-1].pop()    
+            
+            if (stack.length > 0 && stack[stack.length - 1] === doll) {
+                stack.pop()
+                count += 2
+            } else stack.push(doll)
         }
     }
     
